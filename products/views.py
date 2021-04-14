17be8b669e00e6bd20d617fd2090f0d198d227be
Jonathan <jonathanmeaney1982@gmail.com>
2021-04-14 20:05:11 +0000

    Added category, country filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category, Country
 
 
 def all_products(request):
@@ -9,8 +9,20 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
+    countries = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'country' in request.GET:
+            countries = request.GET['country'].split(',')
+            products = products.filter(country__name__in=countries)
+            categories = Country.objects.filter(name__in=countries)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,6 +36,8 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_country': countries
     }
 
     return render(request, 'products/products.html', context)
