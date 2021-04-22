3e479ea4acf0733616230add4d7ebf0349327dff
Jonathan <jonathanmeaney1982@gmail.com>
2021-04-22 11:02:06 +0000

    Added order summary to checkout success / updated brewer-abv fields in product model

--- products/models.py
+++ products/models.py
@@ -41,8 +41,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     size = models.IntegerField()
-    abv = models.DecimalField(max_digits=6, decimal_places=2)
-    brewer = models.CharField(max_length=64, null=True, blank=True)
+    abv = models.DecimalField(max_digits=6, decimal_places=1)
+    brewer = models.CharField(max_length=254, null=False, blank=False)
     image = models.ImageField(null=True, blank=True)
 
     def _generate_sku(self):
