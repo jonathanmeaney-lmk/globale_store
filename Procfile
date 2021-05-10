850886d1e7fa548c0259518c63819ffef3ba8196
Jonathan <jonathanmeaney1982@gmail.com>
2021-05-10 19:19:03 +0000

    heroku deployment part 1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn globale_store.wsgi:application
--- globale_store/settings.py
+++ globale_store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '_gvf)$)1_%fhf17t4t=kd6bz3g6(m2-$ahdoqu%&zenah9g!hl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['globale-beerstore-jm.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,12 +124,17 @@ WSGI_APPLICATION = 'globale_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
