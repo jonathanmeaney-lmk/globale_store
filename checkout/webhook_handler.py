90ae615263a560619f141c4a212bba124496ab03
Jonathan <jonathanmeaney1982@gmail.com>
2021-05-18 20:58:58 +0000

    Fixed webhook bug with logging userprofile  with order

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -81,7 +81,6 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
-                    user_profile=profile,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
@@ -110,6 +109,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
